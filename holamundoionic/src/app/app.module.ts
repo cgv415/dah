562d1b51b345cb7d5ca8fe6767d1c0d520ad581d
cgv415 <carlosgarrido993@gmail.com>
2019-04-15 16:51:01 +0200

    First commit ionic

--- holamundoionic/src/app/app.module.ts
+++ holamundoionic/src/app/app.module.ts
@@ -6,32 +6,40 @@ import { MyApp } from './app.component';
 import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
 import { ItemDetailsPage } from '../pages/item-details/item-details';
 import { ListPage } from '../pages/list/list';
+import { ReposPage } from '../pages/repos/repos';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+import {HttpModule} from '@angular/http';
+import { ReposProvider } from '../providers/repos/repos';
 
 @NgModule({
   declarations: [
     MyApp,
     HelloIonicPage,
     ItemDetailsPage,
-    ListPage
+    ListPage,
+    ReposPage
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HelloIonicPage,
     ItemDetailsPage,
-    ListPage
+    ListPage,
+    ReposPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ReposProvider
   ]
 })
 export class AppModule {}
